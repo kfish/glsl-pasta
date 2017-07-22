4e08f2a854c42d3f2a82d17852e521a07a60d40d
Conrad Parker <conrad@metadecks.org>
2017-07-22 17:37:07 +0800

    Lighting: add vertex.fragmentNormal

--- GLSLPasta/Lighting.elm
+++ GLSLPasta/Lighting.elm
@@ -3,10 +3,10 @@ module GLSLPasta.Lighting exposing (..)
 {-| Basic lighting
 
 # Vertex shaders
-@docs vertexPosition, vertexReflection
+@docs vertexPosition, vertexReflection, vertexNormal
 
 # Fragment shaders
-@docs fragmentReflection
+@docs fragmentReflection, fragmentNormal
 -}
 
 import GLSLPasta exposing (..)
@@ -88,4 +88,111 @@ fragmentReflection =
             gl_FragColor = fragColor;
             """
         ]
+    }
+
+{-| normal mapping according to:
+<http://www.gamasutra.com/blogs/RobertBasler/20131122/205462/Three_Normal_Mapping_Techniques_Explained_For_the_Mathematically_Uninclined.php?print=1>
+-}
+vertexNormal : Part
+vertexNormal =
+    { id = "lighting.vertexNormal"
+    , dependencies = []
+    , globals =
+        [ Attribute "vec3" "position"
+        , Attribute "vec3" "normal"
+        , Attribute "vec2" "texCoord"
+        , Attribute "vec4" "tangent"
+        , Varying "vec2" "vTexCoord"
+        , Varying "vec3" "vLightDirection"
+        , Varying "vec3" "vViewDirection"
+        , Varying "vec4" "worldPosition"
+        , Uniform "mat4" "modelViewProjectionMatrix"
+        , Uniform "mat4" "modelMatrix"
+        , Uniform "vec3" "lightPosition"
+        , Uniform "vec3" "viewPosition"
+        ]
+    , functions =
+        [ """
+            mat3 transpose(mat3 m) {
+                return mat3(m[0][0], m[1][0], m[2][0],
+                            m[0][1], m[1][1], m[2][1],
+                            m[0][2], m[1][2], m[2][2]);
+            }
+            """
+        ]
+    , splices =
+        [ """
+            vec4 pos = vec4(position, 1.0 );
+            vec3 posWorld = (modelMatrix * pos).xyz;
+
+            // Tangent, Bitangent, Normal space matrix TBN
+            // this isn't entirely correct, it should use the normal matrix
+            // http://www.lighthouse3d.com/tutorials/glsl-12-tutorial/the-normal-matrix/
+            vec3 n = normalize((modelMatrix * vec4(normal, 0.0)).xyz);
+            vec3 t = normalize((modelMatrix * vec4(tangent.xyz, 0.0)).xyz);
+            vec3 b = normalize((modelMatrix * vec4((cross(normal, tangent.xyz) * tangent.w), 0.0)).xyz);
+            mat3 tbn = transpose(mat3(t, b, n));
+            vLightDirection = tbn*(lightPosition - posWorld);
+            vViewDirection = tbn*(viewPosition - posWorld);
+            vTexCoord = texCoord;
+            gl_Position = modelViewProjectionMatrix * pos;
+            worldPosition = gl_Position;
+            """
+        ]
+    }
+
+
+{-| normal mapping according to:
+<http://www.gamasutra.com/blogs/RobertBasler/20131122/205462/Three_Normal_Mapping_Techniques_Explained_For_the_Mathematically_Uninclined.php?print=1>
+-}
+fragmentNormal : Part
+fragmentNormal =
+    { id = "lighting.fragmentNormal"
+    , dependencies = []
+    , globals =
+         [ Uniform "sampler2D" "textureDiff"
+         , Uniform "sampler2D" "textureNorm"
+         , Varying "vec2" "vTexCoord"
+         , Varying "vec3" "vLightDirection"
+         , Varying "vec3" "vViewDirection"
+         , Varying "vec4" "worldPosition"
+         ]
+    , functions = []
+    , splices =
+         [ """
+            vec3 lightDir = normalize(vLightDirection);
+
+            // Local normal, in tangent space
+            vec3 pixelNormal = normalize(texture2D(textureNorm, vTexCoord).rgb*2.0 - 1.0);
+            float lambert = max(dot(pixelNormal, lightDir), 0.0);
+
+
+            // diffuse + lambert
+            vec3 lightIntensities = vec3(1.5, 1.0, 1.0);
+            vec3 diffuseColor = texture2D(textureDiff, vTexCoord).rgb;
+            vec3 diffuse = lambert * diffuseColor * lightIntensities;
+
+            // ambient
+            vec3 ambient = 0.3 * diffuseColor;
+
+            // specular
+            float shininess = 32.0;
+            vec3 viewDir = normalize(vViewDirection);
+            vec3 reflectDir = reflect(-lightDir, pixelNormal);
+            vec3 halfwayDir = normalize(lightDir + viewDir);
+            float spec = pow(max(dot(pixelNormal, halfwayDir), 0.0), shininess);
+            vec3 specular = vec3(0.2) * spec * lightIntensities;
+
+            // attenuation
+            float lightAttenuation = 0.3;
+            float attenuation = 1.0 / (1.0 + lightAttenuation * pow(length(vLightDirection), 2.0));
+
+            vec3 final_color = ambient + (diffuse + specular) * attenuation;
+
+            gl_FragColor = vec4(final_color, 1.0);
+
+            float lightenDistance = worldPosition.w * 0.01;
+            gl_FragColor *= 1.0 - lightenDistance * vec4(0.18, 0.21, 0.24, 0.15);
+            """
+         ]
     }
